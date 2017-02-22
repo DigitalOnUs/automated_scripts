cb7977d297c7c55b563e00e08e67bef63abdfd88
root <root@ip-172-31-32-197.us-west-2.compute.internal>
2017-02-22 17:37:51 +0000

    updated starter

--- startclustr.sh
+++ startclustr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Updating packages and adding new repository
 apt-get update
-apt-get upgrade | yes
+apt-get upgrade -y
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list
 apt-get update
