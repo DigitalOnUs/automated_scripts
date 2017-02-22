4af94c8de0b5a3c84912410862aeedad57f5d8f8
root <root@ip-172-31-32-197.us-west-2.compute.internal>
2017-02-22 17:15:33 +0000

    Files

---
+++ startclustr.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Updating packages and adding new repository
+apt-get update
+apt-get upgrade
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list
+apt-get update
+#Installing kubernetes and docker engine
+apt-get install kubelet kubeadm kubectl kubernetes-cni docker-engine
+#comment kubeadm reset if you wil create a new cluster without erase the actual cluster
+#kubeadm reset all cluster if someone exist
+kubeadm reset
+#Creating new cluster
+kubeadm init 
+#Getting secret token to join into the cluster
+#To get a minion run this command kubeadm join --token=$tokenid  $privateinstanceip
+kubectl -n kube-system get secret clusterinfo -o yaml | grep token-map | awk '{print $2}' | base64 -d | sed "s|{||g;s|}||g;s|:|.|g;s/\"//g;" | xargs echo
+#Installing feature for CNI
+kubectl apply -f https://git.io/weave-kube
+#Creating namespace
+kubectl create namespace devops
+#Adding services
+bash services/addservices.sh
+#Adding Pods
+bash pods/addpods.sh
+
+
+echo "Run kubectl get svc,pods -n devops to see the containers"
