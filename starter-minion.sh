780b7fa831c41ec391ee2ded0511910ba7ba61e3
Jairo Lopez <jaloare@gmail.com>
2017-04-24 11:19:06 -0500

    Update starter-minion.sh

--- starter-minion.sh
+++ starter-minion.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #Updating packages and adding new repository
+apt-get update && apt-get install -y apt-transport-https=1.2.19
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
 apt-get update
-apt-get upgrade -y
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list
-apt-get update
+
 #Installing kubernetes and docker engine
-apt-get install -y kubelet nfs-common kubeadm kubectl kubernetes-cni docker.io git
-
-
-
-
+apt-get install -y docker-engine
+apt-get install -y kubelet=1.6.1-00 kubeadm=1.6.1-00 kubectl=1.6.1-00 kubernetes-cni=0.5.1-00
